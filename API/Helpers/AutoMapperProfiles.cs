230b67c944434c549fdf89d2618f4fee5e86419e
Juantrevi <juantrevi70@gmail.com>
2023-12-31 20:20:44 +0000

    -- Business logic moved to services cleaning controllers --

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -13,11 +13,15 @@ public class AutoMapperProfiles : Profile
             .ForMember(dest => dest.PhotoUrl,
                 opt => opt.MapFrom(src => src.Photo.Url))
             .ForMember(dest => dest.FullName,
-                opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
-            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.ContactDetail.Country))
-            .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.ContactDetail.City))
-            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.ContactDetail.PhoneNumber))
-            .ForMember(dest => dest.Roles, opt => opt.MapFrom(src => src.UserRoles));
+                opt => opt.MapFrom(src =>
+                    $"{src.FirstName} {src.LastName}"))
+            .ForMember(dest => dest.Country, opt =>
+                opt.MapFrom(src => src.ContactDetail.Country))
+            .ForMember(dest => dest.City, opt =>
+                opt.MapFrom(src => src.ContactDetail.City))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.ContactDetail.PhoneNumber));
+        
+        
         CreateMap<RegisterDto, AppUser>();
     }
 }
