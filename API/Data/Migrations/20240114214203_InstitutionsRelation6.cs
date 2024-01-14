1978c307c3c92b2de487985d50dc89bb566cf09c
Juantrevi <juantrevi70@gmail.com>
2024-01-14 23:30:12 +0000

    -- Reset Password totally working --

---
+++ API/Data/Migrations/20240114214203_InstitutionsRelation6.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulbEd.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstitutionsRelation6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResetToken",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResetToken",
+                table: "AspNetUsers");
+        }
+    }
+}
